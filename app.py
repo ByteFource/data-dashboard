9fe665826ac7306e4ccf9bad0659b5352249d952
Khaled Ahmed <khaled@Khaleds-MacBook-Pro.local>
2023-10-06 15:18:58 -0400

    Line Comparison

--- app.py
+++ app.py
@@ -3,27 +3,50 @@ import pandas as pd
 import numpy as np  # not explicitly used in this code
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import json
+import plotly
 
 app = Flask(__name__)
 
-df = pd.read_csv('data/MTA_Subway_Terminal_On-Time_Performance__2015-2019.csv')
-df['date'] = pd.to_datetime(df['month'], format='%Y-%m')
-df['year'] = df['date'].dt.year
-df['month'] = df['date'].dt.month
+df = pd.read_csv("data/MTA_Subway_Terminal_On-Time_Performance__2015-2019.csv")
+df["date"] = pd.to_datetime(df["month"], format="%Y-%m")
+df["year"] = df["date"].dt.year
+df["month"] = df["date"].dt.month
 
 
-@app.route('/')
+@app.route("/")
 def dashboard():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/data-overview')
+
+@app.route("/data-overview")
 def data_overview():
-    
     total_records = len(df)
-    start_year = df['year'].min()
-    end_year = df['year'].max()
-    
-    return render_template('data_overview.html', total_records=total_records, start_year=start_year, end_year=end_year) 
+    start_year = df["year"].min()
+    end_year = df["year"].max()
 
-if __name__ == '__main__':
+    return render_template(
+        "data_overview.html",
+        total_records=total_records,
+        start_year=start_year,
+        end_year=end_year,
+    )
+
+
+@app.route("/line-comparison")
+def line_overview():
+    line = df["line"].unique()
+    onTime = df.groupby(["line"])["terminal_on_time_performance"].mean().sort_values()
+    onTime = onTime * 100
+    
+    fig = px.bar(data_frame=onTime)
+    graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+   
+    print(onTime)
+    return render_template("line_comparison.html", graph_json=graph_json)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
